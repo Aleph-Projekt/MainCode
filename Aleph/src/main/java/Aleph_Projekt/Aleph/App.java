50c49665113cb4a66b240456175201139ba36b07
Baron <Baron@Lenovo-PC>
2014-12-20 13:54:40 +0100

    teest2

---
+++ Aleph/src/main/java/Aleph_Projekt/Aleph/App.java
@@ -0,0 +1,13 @@
+package Aleph_Projekt.Aleph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
